46568ab7e856a0912cf4c237ec08c5a17a475504
jiaqi2019 zhang <879632300@qq.com>
2023-11-02 02:30:28 +0000

    move

--- apps/koa/src/token.js
+++ apps/koa/src/token.js
@@ -7,5 +7,4 @@ import crypto, {
 
 console.log(crypto.randomBytes(64).toString('hex'));
 console.log(crypto.randomBytes(64).toString('hex'));
-console.log(crypto.randomBytes(64).toString('hex'));
 console.log(createSecretKey(Buffer.from('我是谁')).type);
